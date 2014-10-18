ca966917abd87c0931dec3e771c3383130e01109
Robert Romano <rromano@gmail.com>
2014-10-18 10:19:04 -0700

    Fixes for failing python3 build

--- setup.py
+++ setup.py
@@ -2,26 +2,20 @@
 
 """Distutils setup file"""
 
-import ez_setup
-ez_setup.use_setuptools()
-
 from setuptools import setup, find_packages
 
 # Metadata
-PACKAGE_NAME = "wsgiref"
-PACKAGE_VERSION = "0.1.2"
+PACKAGE_NAME = "wsgiref-py33"
+PACKAGE_VERSION = "0.1.2-1"
 
 setup(
     name=PACKAGE_NAME,
     version=PACKAGE_VERSION,
-
     description="WSGI (PEP 333) Reference Library",
     author="Phillip J. Eby",
     author_email="web-sig@python.org",
     license="PSF or ZPL",
-
-    url="http://cheeseshop.python.org/pypi/wsgiref",
-
+    url="https://github.com/robromano/wsgiref-py33",
     long_description = open('README.txt').read(),
     test_suite  = 'test_wsgiref',
     packages    = ['wsgiref'],
