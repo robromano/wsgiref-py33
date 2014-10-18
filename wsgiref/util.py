2bef1e6f0f62bfd107fcba5a0dd7c63430a0da5c
Robert Romano <rromano@gmail.com>
2014-10-18 10:25:54 -0700

    Fix has_key using a lambda for python3

--- wsgiref/util.py
+++ wsgiref/util.py
@@ -162,11 +162,11 @@ def setup_testing_defaults(environ):
 
 
 
-_hoppish = {
+_hoppish = lambda key: key in {
     'connection':1, 'keep-alive':1, 'proxy-authenticate':1,
     'proxy-authorization':1, 'te':1, 'trailers':1, 'transfer-encoding':1,
     'upgrade':1
-}.has_key
+}
 
 def is_hop_by_hop(header_name):
     """Return true if 'header_name' is an HTTP/1.1 "Hop-by-Hop" header"""
